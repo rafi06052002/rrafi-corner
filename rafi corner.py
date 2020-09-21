a474aa42301fa77268303bb828dc246d1deff580
rafi <71486441+rafi06052002@users.noreply.github.com>
2020-09-21 22:49:01 +0700

    Add files via upload

---
+++ rafi corner.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 15:16:08 2020
+
+@author: Rafi Muzakki
+"""
+
+
+    
+    
+
+              
+      
+              
+      
+#number 1
+pi=3.14
+degrees=int(input('angle:'))
+radians=degrees*pi/180
+print('degrees:'+ str(degrees))
+print('radians:'+ str(radians))
+
+
+#number 2
+print('\n')
+students1=float( input('student scores:'))
+students2=float( input('student scores:'))
+students3=float( input('student scores:'))
+print('\n')
+average = (students1 + students2 + students3)/3
+print('average:'+ str(average))
+
+
+#number3
+
+class1groupie,class1leftovers=divmod(32,5)
+class2groupie,class2leftovers=divmod(45,7)
+class3groupie,class3leftovers=divmod(51,6)
+print('number of students in each group:')
+print('class 1:',class1groupie)
+print('class 2:',class2groupie)
+print('class 3:',class3groupie)
+print('number of students leftover:')
+print('class 1:',class1leftovers)
+print('class 2:',class2leftovers)
+print('class 3:',class3leftovers)
+
+#number 4
+pi=3.14
+pie_diameter=55.4
+pie_radius=pie_diameter/2
+circumference=2*pi*pie_radius
+circumferencemsg= "Jimmy's pie has a circumference:"
+print(circumferencemsg,circumference)
+
+
+#number 5
+
+velocity=int(input('velocity'))
+frequency=int(input('frequency'))
+wavelength=velocity/frequency
+print('The velocity(m/s)=:',velocity)
+print('The frequency(Hz)=:',frequency)
+print('The wavelength(m)=:',wavelength)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#number 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+      
